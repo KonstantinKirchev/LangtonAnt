9e8ec9e5e34356d57073aea08dbde64447f803a5
Konstantin Kirchev <konstantin1230@yahoo.com>
2023-02-20 12:11:47 +0200

    Add square structure

---
+++ LangtonAnt/LangtonAnt/Square.cs
@@ -0,0 +1,13 @@
+namespace LangtonAnt
+{
+    public struct Square
+    {
+        public int _x, _y;
+
+        public Square(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+    }
+}
