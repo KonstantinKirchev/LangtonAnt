9f3994db41d81303ea75b377865a843b4f7b5ac5
Konstantin Kirchev <konstantin1230@yahoo.com>
2023-02-20 12:58:08 +0200

    Drawing on the canvas

--- LangtonAnt/LangtonAnt/Program.cs
+++ LangtonAnt/LangtonAnt/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Welcome to Langton Ant game.");
+﻿namespace LangtonAnt
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Langton Ant game.");
+
+            LangtonAnt ant = new LangtonAnt(100, 100);
+
+            while (!ant.OutOfBounds)
+            {
+                ant.Step();
+            }
+
+            for (int row = 0; row < 100; row++)
+            {
+                for (int col = 0; col < 100; col++)
+                {
+                    Console.Write(ant.IsBlack[col, row] ? "#" : "-");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Thanks for playing our game.");
+        }
+    }
+}
+
+
+
